eb68db2ecc8c7b97550c0acd13e271eac54a6fe9
kwh0163 <105360209+hotteok13@users.noreply.github.com>
2022-10-18 21:43:20 +0900

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int Num = 0;
+	cin >> Num;
+
+	if (Num / 10 == 0)
+	{
+		cout << Num << endl;
+	}
+	else if (Num / 100 == 0)
+	{
+		cout << Num << endl;
+	}
+	else if (Num / 1000 == 0)
+	{
+		int answer = 99;
+		int FirstNum = Num % 10;
+		int SecondNum = Num 
+	}
+}
